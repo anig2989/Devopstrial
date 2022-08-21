5087ceb88e10465bb19eb26643d1af27ea59184e
anig2989 <82942053+anig2989@users.noreply.github.com>
2022-08-21 08:17:37 +0530

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+SOURCE_REPO_URL="https://$REPO_SCOPED_TOKEN@github.com/anig2989/Devopstrial.git"
+NAME_OF_USER="Anirban Hazra"
+EMAIL_OF_USER_FOR_DESTINATION_REPO="anig2989@gmail.com"

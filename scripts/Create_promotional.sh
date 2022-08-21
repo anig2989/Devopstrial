4e75eb620eb227b0ba5a30debd105203e174adb4
anig2989 <82942053+anig2989@users.noreply.github.com>
2022-08-22 01:54:46 +0530

    Update Create_promotional.sh

--- scripts/Create_promotional.sh
+++ scripts/Create_promotional.sh
@@ -1,8 +1,10 @@
 #using config.sh as source file
-source ./config.sh
-SOURCE_REPO_URL="https://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@github.com/anig2989/Devopstrial.git"
+#source ./config.sh
+SOURCE_REPO_URL="https://github.com/anig2989/Devopstrial.git"
 NAME_OF_USER="Anirban Hazra"
 EMAIL_OF_USER_FOR_DESTINATION_REPO="anig2989@gmail.com"
+CURRENT_BRANCH_NAME='preci'
+DESTINATION_BRANCH_NAME='CI'
 
 #defing local repo directory in variable after cloning.
 SOURCE_DIRECTORY=`basename "${SOURCE_REPO_URL}" .git`

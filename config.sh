9c9d9a94d82d9a11a7ecbef6446ac5a65e54978a
anig2989 <82942053+anig2989@users.noreply.github.com>
2022-08-23 06:17:58 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-SOURCE_REPO_URL="https://anig2989:$PASSWORD@github.com/anig2989/Devopstrial.git"
+SOURCE_REPO_URL="https://github.com/anig2989/Devopstrial.git"
 NAME_OF_USER="Anirban Hazra"
 EMAIL_OF_USER_FOR_DESTINATION_REPO="anig2989@gmail.com"
 #CURRENT_BRANCH_NAME='preci'

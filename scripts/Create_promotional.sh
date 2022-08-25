493c871bc8d6824e09c6031b3aeedaf639a5571b
anig2989 <82942053+anig2989@users.noreply.github.com>
2022-08-26 04:26:49 +0530

    Update Create_promotional.sh

--- scripts/Create_promotional.sh
+++ scripts/Create_promotional.sh
@@ -5,53 +5,38 @@ NAME_OF_USER="anig2989"
 EMAIL_OF_USER_FOR_DESTINATION_REPO="anig2989@gmail.com"
 #CURRENT_BRANCH_NAME='preci'
 #DESTINATION_BRANCH_NAME='CI'
-
 #defing local repo directory in variable after cloning.
 SOURCE_DIRECTORY=`basename "${SOURCE_REPO_URL}" .git`
-
 echo $CURRENT_BRANCH_NAME
-
 #cloning the repo.
 echo git clone ${SOURCE_REPO_URL}
 git clone ${SOURCE_REPO_URL}
-
 #Entering into directory where the repo is cloned.
 echo cd ${SOURCE_DIRECTORY} 
 cd ${SOURCE_DIRECTORY}
-
 echo git fetch origin
 git fetch origin
-
 destination_exists=$(git ls-remote --heads origin $DESTINATION_BRANCH_NAME | wc -l)
-          
 if [ $destination_exists == 0 ]; then
   echo "*************  No $DESTINATION_BRANCH_NAME branch exists  *************"
   exit 1
 fi
-
-current_exists=$(git ls-remote --heads origin $CURRENT_BRANCH_NAME | wc -l)
-          
+current_exists=$(git ls-remote --heads origin $CURRENT_BRANCH_NAME | wc -l)        
 if [ $current_exists == 0 ]; then
   echo "*************  No $CURRENT_BRANCH_NAME branch exists  *************"
   exit 1
 fi
-
 #Creating Promotional branch from destination branch(like CI, QA or UAT).
 echo git checkout $DESTINATION_BRANCH_NAME
 git checkout $DESTINATION_BRANCH_NAME
-
 echo git checkout -b Promotional-$DESTINATION_BRANCH_NAME-${GITHUB_RUN_ID}-${GITHUB_RUN_NUMBER} $DESTINATION_BRANCH_NAME
 git checkout -b Promotional-$DESTINATION_BRANCH_NAME-${GITHUB_RUN_ID}-${GITHUB_RUN_NUMBER} $DESTINATION_BRANCH_NAME
-
 git checkout Promotional-$DESTINATION_BRANCH_NAME-${GITHUB_RUN_ID}-${GITHUB_RUN_NUMBER}
-
 git config --global user.email "${EMAIL_OF_USER_FOR_DESTINATION_REPO}"
 git config --global user.name "${NAME_OF_USER}"
-
 git add .
 git commit -m "creating promotional branch"
 git push origin Promotional-$DESTINATION_BRANCH_NAME-${GITHUB_RUN_ID}-${GITHUB_RUN_NUMBER}
-
 exec_cmd()
 {
   echo "$1"
@@ -64,7 +49,6 @@ exec_cmd()
     do
       echo "grep -lr '<<<<<<<' force-app/ | xargs -d git checkout --theirs"
       grep -lr '<<<<<<<' force-app/ | xargs -d"\n" git checkout --theirs
-
       grep -lrI '<<<<<<<' force-app/
       conflicts_exists=$?
     done
@@ -80,11 +64,9 @@ exec_cmd()
 }
 echo git merge origin/$CURRENT_BRANCH_NAME
 exec_cmd "git merge origin/$CURRENT_BRANCH_NAME"
-
 #Merging current branch with Promotional branch.
 #echo git merge origin/$CURRENT_BRANCH_NAME
 #git merge origin/$CURRENT_BRANCH_NAME
-
 #Pushing Promotional branch.
 exec_cmd()
 {
@@ -92,11 +74,9 @@ exec_cmd()
   $1
   return_code=$?
   if [ $return_code -ne 0 ] ; then
-      
     echo "ERROR - $1 failed with $return_code"
     echo git checkout $DESTINATION_BRANCH_NAME
     git checkout $DESTINATION_BRANCH_NAME
-
     git checkout -d Promotional-$DESTINATION_BRANCH_NAME-${GITHUB_RUN_ID}-${GITHUB_RUN_NUMBER}
     git push origin --delete Promotional-$DESTINATION_BRANCH_NAME-${GITHUB_RUN_ID}-${GITHUB_RUN_NUMBER}
     exit 1
